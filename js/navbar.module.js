1fc669e6d423d8ad81a4a8d2c74cd305a2e43472
Samar-Elfeky <samarelfeky69@gmail.com>
2025-04-29 11:16:09 +0300

    create header section

--- js/navbar.module.js
+++ js/navbar.module.js
@@ -1,9 +1,12 @@
+// navigation bar  ............................................................//
 export function navbar(){
     let navbarList=document.querySelector(".icons .fa-bars");
     let closedNavbar=document.querySelector(".icons .fa-xmark");
     let sidebarIcon=document.getElementById("list-navbar");
     let closeSidebar=document.getElementById("close-side-bar");
     let sidebar=document.querySelector(".side-bar");
+    let navbarContent=document.querySelector(".navbar-content");
+    // navigation bar links in small screens ..................//
     navbarList.addEventListener("click",()=>{
     let navbar=document.querySelector(".navbar-links");
     navbar.style.display="block";
@@ -15,11 +18,21 @@ export function navbar(){
     navbar.style.display="none";
     navbarList.style.display="block";
     closedNavbar.style.display="none";
-    })
+    });
+    // side bar ................................................//
     sidebarIcon.addEventListener("click",()=>{
         sidebar.style.right="0%";
-    })
+    });
     closeSidebar.addEventListener("click",()=>{
-        sidebar.style.right="-150%";
-    })
+        sidebar.style.right="-200%";
+    });
+    // navigation bar opacity change  ..........................//
+    window.addEventListener("scroll", () => {
+        let scrollPosition = window.scrollY;
+        if (scrollPosition < 10 || scrollPosition >= 1500) {
+          navbarContent.style.backgroundColor = "black"; 
+        } else if (scrollPosition >= 10 && scrollPosition < 1500) {
+          navbarContent.style.backgroundColor = "rgba(0, 0, 0, 0.7)";
+        }
+    });
 }
--- script.js
+++ script.js
@@ -1,2 +1,9 @@
-import { navbar } from "./navbar.module.js";
-navbar();
+import { navbar } from "./js/navbar.module.js";
+document.addEventListener("DOMContentLoaded", () => {
+    fetch("navbar.html")
+      .then(response => response.text())
+      .then(data => {
+        document.getElementById("navbar").innerHTML = data;
+        navbar();  
+      });
+  });
