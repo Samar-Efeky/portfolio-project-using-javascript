c4ecfd76e926b907f74ad3df44a35519f42baed9
Samar-Elfeky <samarelfeky69@gmail.com>
2025-05-04 18:46:10 +0300

    add section

--- script.js
+++ script.js
@@ -29,21 +29,21 @@ document.addEventListener("DOMContentLoaded", () => {
       entries.forEach(entry => {
         const ratio = entry.intersectionRatio;
   
-        if (entry.isIntersecting && ratio === 1) {
+        if (ratio === 1) {
           entry.target.classList.add('show');
-        } 
-        else if (ratio < 0.2) {
+        } else if (ratio < 0.5) {
           entry.target.classList.remove('show');
         }
       });
     }, {
-      threshold: Array.from({ length: 101 }, (_, i) => i / 100) 
+      threshold: Array.from({ length: 101 }, (_, i) => i / 100) // [0, 0.01, ..., 1]
     });
   
     sectionView.forEach(section => {
       observer.observe(section);
     });
   }
+  
   
   fetch("footer.html")
   .then(response => response.text())
